b675a325aba6025dc5cee58ae5f92712597b14fd
Justin Smith <justnwsmith@gmail.com>
2025-12-22 16:52:07 -1000

    Removed ICS414 final exam paper

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MainLayout from "./components/layout/MainLayout";
 import ProjectsArchive from "./pages/ProjectsArchive";
-import FinalExam from "./pages/FinalExam";
 import CustomMemoryAllocator from "./pages/projects/CustomMemoryAllocator";
 import "./styles/global.css";
 
@@ -11,7 +10,6 @@ export default function App() {
             <Routes>
                 <Route path="/" element={<MainLayout />} />
                 <Route path="/projects-archive" element={<ProjectsArchive />} />
-                <Route path="/ics414-finalexam" element={<FinalExam />} />
                 <Route path="projects/custom-memory-allocator" element={<CustomMemoryAllocator />} />
             </Routes>
         </Router>
